e296fb27dbd862988cf8d339834d950d510dba3c
Agustin Zelaya <atteagus0418@gmail.com>
2026-06-03 14:52:01 -0600

    se agregaron las inyecciones de cliente en Program

--- inaApi/Program.cs
+++ inaApi/Program.cs
@@ -9,6 +9,8 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 builder.Services.AddScoped<IProductoService, ProductoService>();
 builder.Services.AddScoped<IProductoRepository, ProductoRepository>();
+builder.Services.AddScoped<IClienteService, ClienteService>();
+builder.Services.AddScoped<IClienteRepository, ClienteRepository>();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
