41fecff5604717b936fea279654138ce70e5d927
Agustin Zelaya <atteagus0418@gmail.com>
2026-06-03 14:30:26 -0600

    creacion entidad cliente con sus propiedades

---
+++ inaApp.Entities/Cliente.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace inaApp.Entities
+{
+    internal class Cliente
+    {
+        //propiedades de clientes
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Apellido1 { get; set; }
+        public string Apellido2 { get; set; }
+        public DateOnly FechaNacimiento { get; set; }
+        public bool Estado { get; set; }
+    }
+}
