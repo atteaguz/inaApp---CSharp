4b47797908fd25dd9f1948ef00ac86641afb4ac7
Agustin Zelaya <atteagus0418@gmail.com>
2026-06-03 14:46:57 -0600

    creacion de ClienteService y ClienteRepository usando las interfaces creadas

--- inaApp.Repository/ClienteRepository.cs
+++ inaApp.Repository/ClienteRepository.cs
@@ -1,10 +1,36 @@
-﻿using System;
+﻿using inaApp.Common.interfaces;
+using inaApp.Entities;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace inaApp.Repository
 {
-    public class ClienteRepository
+    public class ClienteRepository : IClienteRepository
     {
+        public Task<Cliente> ActualizarAsync(Cliente cliente)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Cliente> CrearAsync(Cliente cliente)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> EliminarAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<Cliente>> ObtenerPorIdsAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<Cliente>> ObtenerTodosAsync()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
--- inaApp.Services/ClienteService.cs
+++ inaApp.Services/ClienteService.cs
@@ -1,10 +1,36 @@
-﻿using System;
+﻿using inaApp.Common.interfaces;
+using inaApp.Entities;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace inaApp.Services
 {
-    public class ClienteService
+    public class ClienteService : IClienteService
     {
+        public Task<Cliente> ActualizarAsync(Cliente cliente)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Cliente> CrearAsync(Cliente cliente)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> EliminarAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<Cliente>> ObtenerPorIdsAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<Cliente>> ObtenerTodosAsync()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
