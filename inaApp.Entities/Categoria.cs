20fa2136eb4064c994ef08c1a386316ccb7599c5
Agustin Zelaya <atteagus0418@gmail.com>
2026-06-16 14:19:42 -0600

    correcciones

--- inaApp.Entities/Categoria.cs
+++ inaApp.Entities/Categoria.cs
@@ -6,18 +6,18 @@ using System.Text;
 
 namespace inaApp.Entities
 {
-    [Table ("tbCategoria")]
+    [Table("tbCategoria")]
     public class Categoria
     {
         [Required]
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
-        public int Id { get; set; }​
+        public int Id { get; set; }
 
         [Required]
-        [StringLength (150, ErrorMessage = "El nombre no puede superar los 150 caracteres.")]
-        public string Nombre { get; set; }​
+        [StringLength(150, ErrorMessage = "El nombre no puede superar los 150 caracteres.")]
+        public string Nombre { get; set; }
 
-        //relacion entre Categoria y Producto - 1 a *
-        public ICollection<Producto> Productos​ { get; set; }​ = new List<Producto>​
+        // relacion entre Categoria y Producto - 1 a *
+        public ICollection<Producto> Productos { get; set; } = new List<Producto>();
     }
 }
