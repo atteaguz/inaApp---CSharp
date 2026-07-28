6393bf34b0c76ac997b86c4ab83f59e02ca034ef
Agustin Zelaya <atteagus0418@gmail.com>
2026-07-28 14:53:34 -0600

    agregados nuevos enums para impuestos y documentos

---
+++ inaApp.Common/Enums/TipoDocumentoEnum.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace inaApp.Common.Enums
+{
+    public enum TipoDocumentoEnum
+    {
+        FacturaElectronica = 1,
+        NotaCreditoElectronica = 2
+    }
+}
---
+++ inaApp.Common/Enums/TipoImpuestoEnum.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace inaApp.Common.Enums
+{
+    public enum TipoImpuestoEnum
+    {
+        Exento = 0,
+        IVA = 1,
+        Selectivo = 2
+    }
+}
